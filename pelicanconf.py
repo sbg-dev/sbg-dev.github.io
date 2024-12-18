d1db2d108b695cc1e51829ddaf3596108205a41f
Sebastian Gnip <sebastian.gnip@baslerweb.com>
2024-12-18 16:58:43 +0100

    Add Pelican configuration and task management files

---
+++ pelicanconf.py
@@ -0,0 +1,23 @@
+AUTHOR = 'Sebastian Gnip'
+SITENAME = 'sbg-blog'
+SITEURL = ""
+
+PATH = "content"
+PAGE_PATHS = ['pages']
+ARTICLE_PATHS = ['articles']
+OUTPUT_PATH = 'output/'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'en'
+
+THEME = "./christmas-theme"
+
+MENUITEMS = [("About", "/pages/about")]
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
